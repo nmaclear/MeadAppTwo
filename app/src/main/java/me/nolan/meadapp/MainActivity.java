79c4faaaa447f28619b5d7c363a84b173bc27ca8
BuildTools <unconfigured@null.spigotmc.org>
2020-10-24 10:28:44 -0400

    Firebase test working

--- app/src/main/java/me/nolan/meadapp/MainActivity.java
+++ app/src/main/java/me/nolan/meadapp/MainActivity.java
@@ -1,9 +1,13 @@
 package me.nolan.meadapp;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.preference.PreferenceManager;
 
+import android.app.Activity;
 import android.content.Context;
+import android.content.Intent;
 import android.content.SharedPreferences;
 import android.graphics.drawable.Drawable;
 import android.media.MediaPlayer;
@@ -13,6 +17,14 @@ import android.util.Log;
 import android.view.View;
 import android.widget.ImageView;
 
+import com.google.firebase.database.ChildEventListener;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.ArrayList;
 import java.util.Calendar;
 import java.util.Date;
 import java.util.Map;
@@ -20,114 +32,86 @@ import java.util.Random;
 import java.util.prefs.PreferenceChangeEvent;
 
 public class MainActivity extends AppCompatActivity {
-
-    SharedPreferences sp;
+    FirebaseDatabase database = FirebaseDatabase.getInstance();
+    DatabaseReference myRef = database.getReference("logRoll");
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        sp =  getSharedPreferences("settingsConf", MODE_PRIVATE);
         setContentView(R.layout.activity_main);
-        SharedPreferences.Editor spe = sp.edit();
-        Log.i("info", sp.toString());
-        Date currentTime = Calendar.getInstance().getTime();
-        spe.putString("key1", "test1");
-        spe.putString("key2", "test2");
-        spe.putString("key3", "test3");
-        spe.putString("key4", "test4");
-        spe.putString("key5", "test5");
-        spe.putString("key6", "test6");
-        spe.commit();
     }
 
     public void rollDice(View v) {
-        final Handler handler = new Handler();
-        final int delay = 200; //milliseconds
+        int finalVal = 0;
+        int rollTime = 0;
+        //Die faces
+        int[] firstRoll = {1, 2, 2, 3, 3, 3};
+        int[] secondRoll = {0, 0, 1, 2, 3, 4};
+        int[] thirdRoll = {0, 1, 2, 3, 4, 5};
+        ImageView die1 = (ImageView) findViewById(R.id.die1);
+        ImageView die2 = (ImageView) findViewById(R.id.die2);
+        ImageView die3 = (ImageView) findViewById(R.id.die3);
+        int dieRandom1 = getRandom(firstRoll);
+        int dieRandom2 = getRandom(secondRoll);
+        int dieRandom3 = getRandom(thirdRoll);
+        finalVal = finalVal + dieRandom2;
+        finalVal = finalVal + dieRandom3;
 
-        final MediaPlayer mp = MediaPlayer.create(this, R.raw.bell);
-        final int finalVal = 0;
+        if (dieRandom1 == 3) {
+            finalVal += 30;
+            die1.setImageResource(R.drawable.id30);
+        }
+        if (dieRandom1 == 2) {
+            finalVal += 20;
+            die1.setImageResource(R.drawable.id20);
+        }
+        if (dieRandom1 == 1) {
+            finalVal += 10;
+            die1.setImageResource(R.drawable.id10);
+        }
 
-        handler.postDelayed(new Runnable(){
-            int finalVal = 0;
-            int rollTime = 0;
-            //Die faces
-            int[] firstRoll = {1,2,2,3,3,3};
-            int[] secondRoll = {0,0,1,2,3,4};
-            int[] thirdRoll = {0,1,2,3,4,5};
-            int lastRando1 = 1;
-            int lastRando2 = 1;
-            int lastRando3 = 1;
-            public void run(){
-                ImageView die1 = (ImageView) findViewById(R.id.die1);
-                ImageView die2 = (ImageView) findViewById(R.id.die2);
-                ImageView die3 = (ImageView) findViewById(R.id.die3);
-                int dieRandom1 = new Random().nextInt(firstRoll.length);
-                int dieRandom2 = new Random().nextInt(secondRoll.length);
-                int dieRandom3 = new Random().nextInt(thirdRoll.length);
-                finalVal = finalVal+dieRandom2;
-                finalVal = finalVal+dieRandom3;
+        if (dieRandom2 == 0)
+            die2.setImageResource(R.drawable.id0);
+        if (dieRandom2 == 1)
+            die2.setImageResource(R.drawable.id1);
+        if (dieRandom2 == 2)
+            die2.setImageResource(R.drawable.id2);
+        if (dieRandom2 == 3)
+            die2.setImageResource(R.drawable.id3);
+        if (dieRandom2 == 4)
+            die2.setImageResource(R.drawable.id4);
 
+        if (dieRandom3 == 0)
+            die3.setImageResource(R.drawable.id0);
+        if (dieRandom3 == 1)
+            die3.setImageResource(R.drawable.id1);
+        if (dieRandom3 == 2)
+            die3.setImageResource(R.drawable.id2);
+        if (dieRandom3 == 3)
+            die3.setImageResource(R.drawable.id3);
+        if (dieRandom3 == 4)
+            die3.setImageResource(R.drawable.id4);
+        if (dieRandom3 == 4)
+            die3.setImageResource(R.drawable.id5);
 
-                //Prevent similar values
-                if(dieRandom1 == lastRando1)
-                    dieRandom1 = new Random().nextInt(firstRoll.length);
-                if(dieRandom2 == lastRando2)
-                    dieRandom2 = new Random().nextInt(secondRoll.length);
-                if(dieRandom3 == lastRando3)
-                    dieRandom3 = new Random().nextInt(thirdRoll.length);
+        Log.i("info", "pressed");
 
-                if(dieRandom1 == 3) {
-                    finalVal += 30;
-                    die1.setImageResource(R.drawable.id30);
-                }
-                if(dieRandom1 == 2) {
-                    finalVal += 20;
-                    die1.setImageResource(R.drawable.id20);
-                }
-                if(dieRandom1 == 1) {
-                    finalVal += 10;
-                    die1.setImageResource(R.drawable.id10);
-                }
-
-                if(dieRandom2 == 0)
-                    die2.setImageResource(R.drawable.id0);
-                if(dieRandom2 == 1)
-                    die2.setImageResource(R.drawable.id1);
-                if(dieRandom2 == 2)
-                    die2.setImageResource(R.drawable.id2);
-                if(dieRandom2 == 3)
-                    die2.setImageResource(R.drawable.id3);
-                if(dieRandom2 == 4)
-                    die2.setImageResource(R.drawable.id4);
-
-                if(dieRandom3 == 0)
-                    die3.setImageResource(R.drawable.id0);
-                if(dieRandom3 == 1)
-                    die3.setImageResource(R.drawable.id1);
-                if(dieRandom3 == 2)
-                    die3.setImageResource(R.drawable.id2);
-                if(dieRandom3 == 3)
-                    die3.setImageResource(R.drawable.id3);
-                if(dieRandom3 == 4)
-                    die3.setImageResource(R.drawable.id4);
-                if(dieRandom3 == 4)
-                    die3.setImageResource(R.drawable.id5);
-
-                //Rolltime = 25 because delay 1/5 second every iteration, therefore 25 roll times = 5 seconds.
-                rollTime++;
-                lastRando1 = dieRandom1;
-                lastRando2 = dieRandom2;
-                lastRando3 = dieRandom3;
-                Date currentTime = Calendar.getInstance().getTime();
-                if(!(rollTime >= 50))
-                    //Multiply by roll time to create "slow down" effect
-                    handler.postDelayed(this, delay+rollTime*10);
-            }
-        }, delay);
-
+        String k = myRef.push().getKey();
+        //Convert to string for simplicity
+        myRef.child(k).setValue(""+finalVal);
+        Log.i("info", "Die Random 1: " + dieRandom1);
+        Log.i("info", "Die Random 2:" + dieRandom2);
+        Log.i("info", "Die random 3: " + dieRandom3);
+        Log.i("info", "Final: " + finalVal);
     }
 
     public void settingsClicked(View v) {
-        setContentView(R.layout.settings_activity);
+        Intent i = new Intent(MainActivity.this, Settings.class);
+        startActivity(i);
+    }
+
+    public static int getRandom(int[] array) {
+        int rnd = new Random().nextInt(array.length);
+        return array[rnd];
     }
 }
--- app/src/main/java/me/nolan/meadapp/Settings.java
+++ app/src/main/java/me/nolan/meadapp/Settings.java
@@ -12,41 +12,101 @@ import android.widget.TableLayout;
 import android.widget.TableRow;
 import android.widget.TextView;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.preference.PreferenceFragmentCompat;
 import androidx.preference.PreferenceManager;
 
+import com.google.firebase.database.ChildEventListener;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
 import java.util.ArrayList;
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 public class Settings extends AppCompatActivity {
 
-    SharedPreferences sp;
+    ArrayList logList = new ArrayList<String>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        sp = getSharedPreferences("settingsConf", MODE_PRIVATE);
         setContentView(R.layout.settings_activity);
-
-        ListView lv = (ListView) findViewById(R.id.listMain);
-
-        ArrayList<String> settingsArray = new ArrayList<String>();
-        Map<String, ?> prefsMap = sp.getAll();
-        for (Map.Entry<String, ?> entry : prefsMap.entrySet()) {
-            settingsArray.add(entry.getValue().toString());
-        }
-        // This is the array adapter, it takes the context of the activity as a
-        // first parameter, the type of list view as a second parameter and your
-        // array as a third parameter.
-        ArrayAdapter<String> arrayAdapter = new ArrayAdapter<String>(this,
-                android.R.layout.simple_list_item_1, settingsArray);
-
-        lv.setAdapter(arrayAdapter);
     }
 
     public void exit(View v) {
         this.finish();
+    }
+
+    FirebaseDatabase database = FirebaseDatabase.getInstance();
+    DatabaseReference myRef = database.getReference("logRoll");
+
+    public void onStart() {
+        // Read from the database
+        super.onStart();
+        myRef.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                Map<String,String> td = (HashMap<String,String>) dataSnapshot.getValue();
+                ArrayList<String> array = new ArrayList<String>();
+                for(Object obj : td.values()) {
+                    array.add(obj.toString());
+                }
+                updateList(array);
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+
+            }
+        });
+
+        myRef.addChildEventListener(new ChildEventListener() {
+            @Override
+            public void onChildAdded(@NonNull DataSnapshot dataSnapshot, @Nullable String s) {
+
+            }
+
+            @Override
+            public void onChildChanged(@NonNull DataSnapshot dataSnapshot, @Nullable String s) {
+
+            }
+
+            @Override
+            public void onChildRemoved(@NonNull DataSnapshot dataSnapshot) {
+
+            }
+
+            @Override
+            public void onChildMoved(@NonNull DataSnapshot dataSnapshot, @Nullable String s) {
+
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+
+            }
+        });
+
+    }
+
+    private void updateList(ArrayList<String> array) {
+        ListView lv = (ListView) findViewById(R.id.listMain);
+
+
+        ArrayAdapter<String> arrayAdapter = new ArrayAdapter<String>(this,
+                android.R.layout.simple_list_item_1, array);
+
+        logList = array;
+
+        lv.setAdapter(arrayAdapter);
     }
 }
